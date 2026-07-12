a80766ce9e6f58a7b71f40fe28740a7c82a641a1
VIVEKDHEDHI021 <vivek.test.8525@gmail.com>
2026-07-12 14:34:39 +0530

    Disable vertical and horizontal scrollbars at WebView level in MainActivity

--- android/app/src/main/java/com/glucolab/app/MainActivity.java
+++ android/app/src/main/java/com/glucolab/app/MainActivity.java
@@ -1,5 +1,16 @@
 package com.glucolab.app;
 
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStart() {
+        super.onStart();
+        WebView webView = this.bridge.getWebView();
+        if (webView != null) {
+            webView.setVerticalScrollBarEnabled(false);
+            webView.setHorizontalScrollBarEnabled(false);
+        }
+    }
+}
